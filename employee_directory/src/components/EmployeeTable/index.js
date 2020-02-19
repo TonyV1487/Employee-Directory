e49fc2409a025a33774b3ba5bf15887c2b572f7c
TonyV <tvester1487@gmail.com>
2020-02-18 21:28:40 -0600

    Add Bootstrap

--- employee_directory/src/components/EmployeeTable/index.js
+++ employee_directory/src/components/EmployeeTable/index.js
@@ -1,13 +1,20 @@
 import React from "react";
-import "./style.css";
 
 function EmployeeTable(props) {
+  const { onSort } = props;
   return (
     <table class="table">
       <thead>
         <tr>
           <th scope="col">#</th>
-          <th scope="col">First</th>
+          <th
+            scope="col"
+            onClick={() => {
+              onSort("firstName");
+            }}
+          >
+            First
+          </th>
           <th scope="col">Last</th>
           <th scope="col">Email</th>
           <th scope="col">Desk Phone</th>
@@ -20,27 +27,3 @@ function EmployeeTable(props) {
 }
 
 export default EmployeeTable;
-
-{
-  /* <div className="card">
-      <div className="content">
-        <ul>
-          <li>
-            <strong>Name:</strong> {props.firstName} {props.lastName}
-          </li>
-          <li>
-            <strong>Email:</strong> {props.email}
-          </li>
-          <li>
-            <strong>Desk:</strong> {props.deskPhone}
-          </li>
-          <li>
-            <strong>Mobile:</strong> {props.mobilePhone}
-          </li>
-        </ul>
-      </div>
-      <span onClick={() => props.removeFriend(props.id)} className="remove">
-        𝘅
-      </span>
-    </div> */
-}
